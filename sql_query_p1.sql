ee07df1714ebb7ca76a12e2b56acf71b6f61a54b
Richmond <162697183+Richmond-Orekyeh1@users.noreply.github.com>
2024-08-27 15:11:53 +0100

    Update sql_query_p1.sql

--- sql_query_p1.sql
+++ sql_query_p1.sql
@@ -6,17 +6,17 @@ CREATE DATABASE sql_project_p2;
 DROP TABLE IF EXISTS retail_sales;
 CREATE TABLE retail_sales
             (
-                transaction_id INT PRIMARY KEY,	
-                sale_date DATE,	 
-                sale_time TIME,	
-                customer_id	INT,
-                gender	VARCHAR(15),
-                age	INT,
-                category VARCHAR(15),	
-                quantity	INT,
-                price_per_unit FLOAT,	
-                cogs	FLOAT,
-                total_sale FLOAT
+            transaction_id INT PRIMARY KEY,	
+            sale_date DATE,	 
+            sale_time TIME,	
+            customer_id INT,
+            gender VARCHAR(15),
+            age INT,
+            category VARCHAR(15),	
+            quantity INT,
+            price_per_unit FLOAT,	
+            cogs FLOAT,
+            total_sale FLOAT
             );
 
 SELECT * FROM retail_sales
@@ -30,64 +30,70 @@ SELECT
 FROM retail_sales
 
 -- Data Cleaning
-SELECT * FROM retail_sales
+SELECT * 
+FROM retail_sales
 WHERE transactions_id IS NULL
 
-SELECT * FROM retail_sales
+SELECT * 
+FROM retail_sales
 WHERE sale_date IS NULL
 
-SELECT * FROM retail_sales
+SELECT * 
+FROM retail_sales
 WHERE sale_time IS NULL
 
-SELECT * FROM retail_sales
+SELECT * 
+FROM retail_sales
 WHERE 
     transaction_id IS NULL
-    OR
+            OR
     sale_date IS NULL
-    OR 
+            OR 
     sale_time IS NULL
-    OR
+            OR
     gender IS NULL
-    OR
+            OR
     category IS NULL
-    OR
+            OR
     quantity IS NULL
-    OR
+            OR
     cogs IS NULL
-    OR
+            OR
     total_sale IS NULL;
     
 -- 
-DELETE FROM retail_sales
+DELETE 
+FROM retail_sales
 WHERE 
     transaction_id IS NULL
-    OR
+            OR
     sale_date IS NULL
-    OR 
+            OR 
     sale_time IS NULL
-    OR
+            OR
     gender IS NULL
-    OR
+            OR
     category IS NULL
-    OR
+            OR
     quantity IS NULL
-    OR
+            OR
     cogs IS NULL
-    OR
+            OR
     total_sale IS NULL;
     
 -- Data Exploration
 
 -- How many sales we have?
-SELECT COUNT(*) as total_sale FROM retail_sales
+SELECT COUNT(*) AS total_sale 
+FROM retail_sales
 
 -- How many uniuque customers we have ?
 
-SELECT COUNT(DISTINCT customer_id) as total_sale FROM retail_sales
+SELECT COUNT(DISTINCT customer_id) AS total_sale 
+FROM retail_sales
 
-
-
-SELECT DISTINCT category FROM retail_sales
+SELECT DISTINCT category 
+FROM retail_sales
 
 
 -- Data Analysis & Business Key Problems & Answers
@@ -120,9 +126,9 @@ SELECT
 FROM retail_sales
 WHERE 
     category = 'Clothing'
-    AND 
+            AND 
     TO_CHAR(sale_date, 'YYYY-MM') = '2022-11'
-    AND
+            AND
     quantity >= 4
 
 
@@ -130,22 +136,23 @@ WHERE
 
 SELECT 
     category,
-    SUM(total_sale) as net_sale,
-    COUNT(*) as total_orders
+    SUM(total_sale) AS net_sale,
+    COUNT(*) AS total_orders
 FROM retail_sales
 GROUP BY 1
 
 -- Q.4 Write a SQL query to find the average age of customers who purchased items from the 'Beauty' category.
 
 SELECT
-    ROUND(AVG(age), 2) as avg_age
+    ROUND(AVG(age), 2) AS avg_age
 FROM retail_sales
 WHERE category = 'Beauty'
 
 
 -- Q.5 Write a SQL query to find all transactions where the total_sale is greater than 1000.
 
-SELECT * FROM retail_sales
+SELECT * 
+FROM retail_sales
 WHERE total_sale > 1000
 
 
@@ -154,7 +161,7 @@ WHERE total_sale > 1000
 SELECT 
     category,
     gender,
-    COUNT(*) as total_trans
+    COUNT(*) AS total_trans
 FROM retail_sales
 GROUP 
     BY 
@@ -172,13 +179,13 @@ SELECT
 FROM 
 (    
 SELECT 
-    EXTRACT(YEAR FROM sale_date) as year,
-    EXTRACT(MONTH FROM sale_date) as month,
-    AVG(total_sale) as avg_sale,
-    RANK() OVER(PARTITION BY EXTRACT(YEAR FROM sale_date) ORDER BY AVG(total_sale) DESC) as rank
+    EXTRACT(YEAR FROM sale_date) AS year,
+    EXTRACT(MONTH FROM sale_date) AS month,
+    AVG(total_sale) AS avg_sale,
+    RANK() OVER(PARTITION BY EXTRACT(YEAR FROM sale_date) ORDER BY AVG(total_sale) DESC) AS rank
 FROM retail_sales
 GROUP BY 1, 2
-) as t1
+) AS t1
 WHERE rank = 1
     
 -- ORDER BY 1, 3 DESC
@@ -187,7 +194,7 @@ WHERE rank = 1
 
 SELECT 
     customer_id,
-    SUM(total_sale) as total_sales
+    SUM(total_sale) AS total_sales
 FROM retail_sales
 GROUP BY 1
 ORDER BY 2 DESC
@@ -198,13 +205,13 @@ LIMIT 5
 
 SELECT 
     category,    
-    COUNT(DISTINCT customer_id) as cnt_unique_cs
+    COUNT(DISTINCT customer_id) AS cnt_unique_cs
 FROM retail_sales
 GROUP BY category
 
 
 
--- Q.10 Write a SQL query to create each shift and number of orders (Example Morning <12, Afternoon Between 12 & 17, Evening >17)
+-- Q.10 Write a SQL query to create each shift and number of orders (Example Morning <12, Afternoon Between 12 & 17, Evening >17).
 
 WITH hourly_sale
 AS
@@ -214,12 +221,12 @@ SELECT *,
         WHEN EXTRACT(HOUR FROM sale_time) < 12 THEN 'Morning'
         WHEN EXTRACT(HOUR FROM sale_time) BETWEEN 12 AND 17 THEN 'Afternoon'
         ELSE 'Evening'
-    END as shift
+    END AS shift
 FROM retail_sales
 )
 SELECT 
     shift,
-    COUNT(*) as total_orders    
+    COUNT(*) AS total_orders    
 FROM hourly_sale
 GROUP BY shift
 
